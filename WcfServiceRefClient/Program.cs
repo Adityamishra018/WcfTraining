9eb2c8db6dcf34e046941cc49d7116e686872988
Aditya Mishra <adi.adityamishra007@gmail.com>
2023-05-21 16:27:18 +0530

    Ref service client added

---
+++ WcfServiceRefClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceRefClient.HelloworldServiceRef;
+
+namespace WcfServiceRefClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new HelloworldServiceClient();
+            Console.WriteLine(service.GetMessage("Aditya"));
+        }
+    }
+}
